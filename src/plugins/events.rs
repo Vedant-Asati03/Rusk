450ea6602f675f681194ea766bdc80060484e685
vedant-asati03 <vedant.asati03@gmail.com>
2025-07-17 01:56:27 +0530

    Add EventBus struct for plugin communication

---
+++ src/plugins/events.rs
@@ -0,0 +1,10 @@
+/// Event system for plugin communication
+pub struct EventBus {
+    // TODO: Implement event bus for plugin communication
+}
+
+impl EventBus {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
